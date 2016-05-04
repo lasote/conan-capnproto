407b5355299d459401cb0ac4fb66e702f28db52f
Luis <lasote@gmail.com>
2016-05-04 10:01:55 +0200

    Better port and kill and dylib copy

--- test/conanfile.py
+++ test/conanfile.py
@@ -25,18 +25,20 @@ class DefaultNameConan(ConanFile):
         self.run("cmake --build . %s" % cmake.build_config)
 
     def imports(self):
-        self.copy(pattern="*.dll", dst="", src="bin")
-        self.copy(pattern="*.dylib", dst="", src="lib")
+        self.copy(pattern="*.dll", dst="bin", src="bin")
+        self.copy(pattern="*.dylib", dst="bin", src="lib")
         
     def test(self):
         process = subprocess.Popen(
-            "cd bin && .%scalculator_server localhost:8888 &" % os.sep, 
+            "cd bin && .%scalculator_server localhost:49993 &" % os.sep, 
             stdout=subprocess.PIPE, 
             stderr=subprocess.PIPE,
             shell=True, 
             preexec_fn=os.setsid
         ) 
         
-        self.run("cd bin && .%scalculator_client localhost:8888" % os.sep)
+        self.run("cd bin && .%scalculator_client localhost:49993" % os.sep)
         
         os.killpg(process.pid, signal.SIGTERM)
+        os.killpg(process.pid, signal.SIGKILL)
+        
